53aebf19af8f291b2ad8fff6def07f7bcf32c9d0
rudrakshi-Dadwal <dadwalrudrakshi@gmail.com>
2026-01-23 14:04:46 +0530

    adding song details in js

--- styling.js
+++ styling.js
@@ -1,4 +1,76 @@
-var nextbtn = document.querySelector("#control .fa-solid.fa-forward-step");
+let songdetails=[
+   { title:"Zen",
+    artist:"Jennie",
 
-var cover = document.getElementById("#albumcover");
+    duration:"3:12"
+    imgsrc:"jennie.jpg"
+    songsrc:"zen.mp3"
+
+},
+{
+    title:"MY Eyes ",
+    artist:"travis Scott",
+  imgsrc:"utopia.jpg"
+    songsrc:"my eyes.mp3"
+    duration:"3:20"
+},
+
+{
+    title:"Humble ",
+    artist:"Kendrick Lamar",
+  imgsrc:"damn.jpg"
+    songsrc:"humble.mp3"
+    duration:"2:46"
+},
+{
+    title:"we can't be friends ",
+    artist:"ariana grande",
+    imgsrc:"sunshine.jpg"
+    songsrc:"we cant be friend.mp3"
+    duration:"4:16"
+},
+{
+    title:"Blind Eyes red ",
+    artist:"Minnie",
+  imgsrc:"minnie.jpg"
+    songsrc:"blind eyes red.mp3"
+    duration:" 3:26"
+},
+{
+    title:"heavy ",
+    artist:"the marias",
+    imgsrc:"marias.jpg"
+    songsrc:"heavy.mp3"
+    duration:"3:20"
+},
+{
+    title:"drama ",
+    artist:"aespa",
+  imgsrc:"aespa.jpg"
+    songsrc:"drama.mp3"
+    duration:"3:20"
+},
+{
+    title:"best ",
+    artist:"gracie abrams",
+  imgsrc:"gracie.jpg"
+    songsrc:"best.mp3"
+    duration:"3:20"
+},
+{
+    title:"bye ",
+    artist:"ariana grande",
+  imgsrc:"sunshine.jpg"
+    songsrc:"bye.mp3"
+    duration:"3:20"
+},{
+    title:"to say hello ",
+    artist:"the marias",    
+    imgsrc:"marias.jpg"
+    songsrc:"to say hello.mp3"
+    duration:"3:20"
+}
+
+
+];
 
